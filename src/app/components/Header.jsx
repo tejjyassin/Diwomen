821a1a18b9a60da8d155a0853985762f440afef9
Yassine Tejjani <tejjyassin@gmail.com>
2024-01-15 03:19:41 +0100

    Updated header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white">
+    <header className="relative z-[2]">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
