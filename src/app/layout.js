75cd00e8d4e75d7b52ea039831ec4fc9b3c33f8e
Yassine Tejjani <tejjyassin@gmail.com>
2024-01-15 03:33:44 +0100

    Added colors

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Diwomen - Under construction',
-  description: 'Diwomen - Under construction',
+  title: 'Diwomen ',
+  description: 'Diwomen ',
 }
 
 export default function RootLayout({ children }) {
